8455f546d04b8be525797c97080256a474fc2764
Joel Goodman <joel@bravery.co>
2022-10-22 10:18:03 -0500

    Fix font issues, update speaking page

---
+++ rollup.config.mjs
@@ -0,0 +1,22 @@
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import path from 'path';
+
+const SRC_DIR = "_includes";
+const ASSETS_DIR = "_includes/assets";
+const DIST_DIR = "_site";
+
+const JS_SRC = path.join(ASSETS_DIR, 'js');
+const JS_DIST = path.join(DIST_DIR, "assets/js");
+
+export default {
+    input: path.join(JS_SRC, 'jgg.js'),
+    output: {
+        file: path.join(JS_DIST, 'jgg.bundle.js'),
+        format: 'iife'
+    },
+    plugins: [
+        resolve(),
+		terser()
+    ]
+}
