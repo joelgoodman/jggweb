dfdea633cfa8debecccee047e80684b891a20c7f
Joel Goodman <joel@bravery.co>
2023-01-08 16:00:33 -0500

    add subscriber-targeted content

--- _includes/assets/js/jgg.js
+++ _includes/assets/js/jgg.js
@@ -1,4 +1,9 @@
 const menu = document.querySelector('.toggle-menu');
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const subscriber = urlParams.get("email_confirmed");
+const subStatus = document.cookie.split("; ").find((row) => row.startsWith("jggsubscriber="))?.split("=")[1];
 
 if ( menu ) {
     menu.addEventListener('click', (e) => {
@@ -50,3 +55,22 @@ function validateEmail(email){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email.toLowerCase());
 }
+
+const disableSubForm = () => {
+    emailField.disabled = true;
+    emailField.placeholder = "Thanks for being a subscriber!";
+    subForm.querySelector('button[type="submit"]').disabled = true;
+
+    document.body.querySelector(".subscribe-message").innerText = "You are already subscribed and your email is confirmed 👍🏼. Thanks for being here!";
+}
+
+
+if ( subscriber === 'true' ) {
+    document.cookie = "jggsubscriber=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; Secure";
+}
+
+if ( subscriber === 'true' || subStatus.length) {
+    document.body.classList.add("subscriber-confirmed");
+    disableSubForm();
+}
+
