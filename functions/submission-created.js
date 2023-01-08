873238e7e535d30809b9c7219e44300c7dce6140
Joel Goodman <joel@bravery.co>
2023-01-08 17:07:15 -0500

    try it a different way

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,59 +1,38 @@
 import {} from 'dotenv/config';
-const fs = require("fs").promises;
-const SibApiV3Sdk = require("sib-api-v3-sdk");
-
-let defaultClient = SibApiV3Sdk.ApiClient.instance;
-let apiKey = defaultClient.authentications["api-key"];
-let apiInstance = new SibApiV3Sdk.ContactsApi();
-
-apiKey.apiKey = process.env.SENDINBLUE;
+import {ApiClient, ContactsApi, authentications, CreateDoiContact, CreateContact} from 'sib-api-v3-sdk';
+import { promises as Fs } from "fs";
 
 const headers = {
 	"Access-Control-Allow-Origin": "*",
 	"Access-Control-Allow-Headers": "Content-Type",
 };
 
-export async function handler(event, context) {
-    let contactObj = {
-		statusCode: 200,
-		headers,
-		response: "Email submitted",
-	};
-	const email = JSON.parse(event.body).payload.email;
-    try {
-		console.log("Add new contact: ", email);
+export async function handler(event) {
+	ApiClient.instance.authentications["api-key"].apiKey = process.env.SENDINBLUE;
+    const email = JSON.parse(event.body).payload.email;
+	const apiInstance = new ContactsApi();
+	const createDoiContact = new CreateDoiContact();
+	console.log(email);
+	createDoiContact.email = email;
+	createDoiContact.attributes =  {};
+	createDoiContact.includeListIds = [4];
+	createDoiContact.templateId = 5;
+	createDoiContact.attributes = {};
+	createDoiContact.redirectionUrl ="https://joelgoodman.co/?email_confirmed=true";
+	console.log("Add new contact: ", email);
 
-		let createDoiContact = new SibApiV3Sdk.CreateDoiContact();
-		createDoiContact.email = email;
-        createDoiContact.attributes =  {};
-		createDoiContact.includeListIds = [4];
-		createDoiContact.templateId = 5;
-		createDoiContact.attributes = {};
-        createDoiContact.redirectionUrl ="https://joelgoodman.co/?email_confirmed=true";
-
-		apiInstance.createDoiContact(createDoiContact).then(
-			function (output) {
-				output = JSON.stringify(output);
-				const message =
-					"API called successfully. Returned data: " + output;
-				console.log(message);
-				contactObj.response = message;
-				return contactObj;
-			},
-			function (error) {
-				console.log(error);
-				contactObj.statusCode = error.status;
-				contactObj.response = error.error;
-				return contactObj;
-			}
-		);
-	    return contactObj;
-	} catch (error) {
-		console.log(error);
-		contactObj.statusCode = 400;
-		contactObj.response = error;
-		return contactObj;
-	} finally {
-		console.log("in finally block");
-	}
+	return apiInstance
+		.createDoiContact(createDoiContact)
+		.then(() => {
+			return {
+				body: JSON.stringify({ message: "Success" }),
+				statusCode: 200,
+			};
+		})
+		.catch((error) => {
+			return {
+				body: JSON.stringify(error),
+				statusCode: 500,
+			};
+		});
 }
